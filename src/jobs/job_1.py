83ae58acb16c8df2681df7b6ce6189e8bef4e17d
Harathi Pasam <codetutelage@gmail.com>
2024-05-30 13:28:41 -0700

    changes to job1 and job2

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -4,7 +4,55 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    with actors_last_yr as (
+  select * from harathi.actors where current_year = 1939
+),
+actors_this_yr as (
+  select
+    actor,
+    actor_id,
+    ARRAY_AGG(
+      ROW(
+        film,
+        film_id,
+        votes,
+        rating,        
+        year
+      )
+    ) AS films,
+    AVG(rating) AS avg_rating,
+    year
+  FROM
+    bootcamp.actor_films
+  WHERE
+    rating is not null
+    and year = 1940
+  GROUP BY
+    actor,
+    actor_id,
+    year
+)
+select
+  coalesce(aly.actor, aty.actor) as actor,
+  coalesce(aly.actor_id, aty.actor_id) as actor_id,
+  case
+    when aty.films is null then aly.films
+    when aty.films is not null
+    and aly.films is null then aty.films
+    when aty.films is not null
+    and aly.films is not null then aty.films || aly.films
+  end as films,
+  CASE
+    WHEN avg_rating > 8 THEN 'star'
+    WHEN avg_rating > 7 THEN 'good'
+    WHEN avg_rating > 6 THEN 'average'
+    ELSE 'bad'
+  END AS quality_class,
+  aty.year is not null as is_active,
+  coalesce(aty.year, aly.current_year + 1) as current_year
+from
+  actors_last_yr aly FULL
+  OUTER JOIN actors_this_yr aty ON aly.actor_id = aty.actor_id
     """
     return query
 
@@ -14,7 +62,7 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "harathi.actors"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -4,7 +4,48 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH actors_lagged AS (
+  SELECT
+    actor,
+    actor_id,
+    quality_class,
+    is_active,
+    LAG(is_active, 1) OVER (
+      PARTITION BY actor_id
+      ORDER BY current_year
+    ) AS is_active_last_year,
+    LAG(quality_class, 1) OVER (
+      PARTITION BY actor_id
+      ORDER BY current_year
+    ) AS quality_class_last_year,
+    current_year
+  FROM harathi.actors
+  WHERE current_year <= 2021 -- Adjusted to include data up to 2021
+),
+streaked AS (
+  SELECT
+    *,
+    SUM(
+      CASE
+        WHEN is_active = is_active_last_year AND quality_class = quality_class_last_year THEN 0
+        ELSE 1
+      END
+    ) OVER (
+      PARTITION BY actor_id
+      ORDER BY current_year
+    ) AS streak_identifier -- This identifier increments when there is a change in 'is_active' or 'quality_class'
+  FROM actors_lagged
+)
+SELECT
+  actor_id,
+  actor,
+  MAX(quality_class) AS quality_class,
+  MAX(is_active) AS is_active,
+  MIN(current_year) AS start_date,
+  MAX(current_year) AS end_date,
+  2021 AS current_year -- Adjusted to 2021
+FROM streaked
+GROUP BY actor_id, actor, streak_identifier
     """
     return query
 
@@ -14,7 +55,7 @@ def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_2(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "harathi.actors_history_scd"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
